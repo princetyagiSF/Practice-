3417455a800240aaf18e9d5f950d0b449c102f58
Prince Tyagi <42377167+princetyagi258456@users.noreply.github.com>
2020-08-29 23:38:18 +0530

    Updation

---
+++ python/string/Updation.py
@@ -0,0 +1,8 @@
+#Deletion and updation from a string
+string2="Hello I am there"
+print("Initial String")
+print(string2)
+# string2[1]='p'#This type of updation not support by string
+string2="Hello You are not there"
+print("\nUpdation string will be")
+print(string2)
