a86636fa52f1b034e8139e3b4097a52678318c55
Prince Tyagi <42377167+princetyagi258456@users.noreply.github.com>
2020-08-29 23:38:57 +0530

    Create Deletion.py

---
+++ python/string/Deletion.py
@@ -0,0 +1,9 @@
+#Deletion of a character
+string3='Hello Google'
+print("Initial String:")
+print(string3)
+
+#del string3[0]//You cannot delete the string in this way
+del string3
+print("After Deletion:")
+print(string3)
