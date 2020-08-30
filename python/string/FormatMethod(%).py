38d409e9aedbc970e74f679091636d4ee05f4191
Prince Tyagi <42377167+princetyagi258456@users.noreply.github.com>
2020-08-30 16:04:05 +0530

    format method by using % operator

---
+++ python/string/FormatMethod(%).py
@@ -0,0 +1,5 @@
+Integer1=12.23434
+print('Formatting in 3.2f format: ')
+print('The value of Integer1 is %.3f ' %Integer1)
+print('Formatting in 3.4f format:')
+print('The value of Integer1 is %.4f' %Integer1)
