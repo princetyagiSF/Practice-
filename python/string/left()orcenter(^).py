ed4e7dff6a3e414a26e0e986a90c55cd833c2184
Prince Tyagi <42377167+princetyagi258456@users.noreply.github.com>
2020-08-30 14:53:23 +0530

    Create left()orcenter(^).py

---
+++ python/string/left()orcenter(^).py
@@ -0,0 +1,12 @@
+String1= '{:}{:^10}{:^5}{:^3}'.format('Hello','Prince','This','Side')
+print("\nLeft, center and right alignment with formatting: ")
+print(String1)
+OUTPUT
+Left, center and right alignment with formatting: 
+Hello  Prince  This Side
+
+
+String2="\n{0:<10} was founded in {1:<4}!".format("HELLO",2020)
+print(String2)
+OUTPUT
+HELLO      was founded in 2020!
