1cb95bab480a25e58b608e759e5c47de7f05b685
Prince Tyagi <42377167+princetyagi258456@users.noreply.github.com>
2020-09-01 02:13:36 +0530

    Basics of dict

---
+++ python/dictionary/dict.py
@@ -0,0 +1,9 @@
+# my_stuff={"key1":123,'key2':456,'key3':789}
+# my_stuff['key1']=10
+# my_stuff={"key1":123,'key2':456,'key3':789,'key4':{'hello':['google','from','india']}}
+# print(my_stuff ['key4']['hello'][2].upper())
+
+my_stuff={'lunch':'Pizza','Breakfast':'burgur'}
+my_stuff['lunch']='Macroni'
+my_stuff['Breakfast']='Pasta'
+print(my_stuff)
