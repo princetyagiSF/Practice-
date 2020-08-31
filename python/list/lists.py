3eadcccadfde0b66fe13d47b217f499d70123063
Prince Tyagi <42377167+princetyagi258456@users.noreply.github.com>
2020-08-31 19:15:57 +0530

    various method in list

---
+++ python/list/lists.py
@@ -0,0 +1,30 @@
+# mylist=[1,2,3,4,[5,6,7],[8,9,10]]
+
+# mylist=['efesd',122,2,34,45.45,'rg']
+
+# mylist=['a','b','c',[1,2,3],['d','e','f']]
+
+# mylist.append('prince')
+
+# mylist.extend('prince')
+
+# mylist.pop(-1)
+
+# mylist.reverse()
+
+# mylist=[3,5,67,2,43,6,1.33,55]
+
+# mylist.sort()
+
+
+#Nested List
+# mylist=[1,2,['x','y','z'],[3,4,5,]]
+# print(mylist[3][2])
+
+#List Comprehension
+
+# matrix=[[1,2,3],[4,5,6],[7,8,9]]
+
+# x=[i[1] for i in matrix]
+# print(x) 
+# Output:-[2, 5, 8]
