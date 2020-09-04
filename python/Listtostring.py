cf55804ad7355ef8f94e876351fe5f4c3ff82ddd
Prince Tyagi <42377167+princetyagi258456@users.noreply.github.com>
2020-09-04 23:33:30 +0530

    convert a list into a string

---
+++ python/Listtostring.py
@@ -0,0 +1,9 @@
+How to convert a list into a string?
+
+A) When we want to convert a list into a string, we can use the <”.join()> method which joins all the elements into one and returns as a string.
+
+Example:
+
+weekdays = ['sun','mon','tue','wed','thu','fri','sat']
+listAsString = ' '.join(weekdays)
+print(listAsString)P
