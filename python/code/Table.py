2da7bf9c0b9b517b2ee12833b8c1b2c18e8fb053
Prince Tyagi <42377167+princetyagi258456@users.noreply.github.com>
2020-09-09 23:51:55 +0530

    Print table

---
+++ python/code/Table.py
@@ -0,0 +1,20 @@
+Write a Python Program to Print Table of a Given Number?
+
+A) Python Program to Print Table of a Given Number:
+
+n=int(input("Enter the number to print the tables for:"))
+for i in range(1,11):
+print(n,"x",i,"=",n*i)
+Output:
+
+Enter the number to print the tables for:7
+7 x 1 = 7
+7 x 2 = 14
+7 x 3 = 21
+7 x 4 = 28
+7 x 5 = 35
+7 x 6 = 42
+7 x 7 = 49
+7 x 8 = 56
+7 x 9 = 63
+7 x 10 = 70
